29b9e5a59c1f0ca19b8f81cecbdaea9aef0999cf
AMO15310 <wachiraamos402@gmail.com>
2023-10-08 12:28:00 +0300

    task-2

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,13 +1,11 @@
-import ClassRoom from "./0-classroom.js"
+import ClassRoom from './0-classroom';
 
-export default function initializeRooms(){
+export default function initializeRooms() {
+  const rooms = [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+  ];
 
-	rooms = [
-
-		new ClassRoom(19),
-		new ClassRoom(20),
-		new ClassRoom(34)
-	]
-
-	return rooms;
+  return rooms;
 }
